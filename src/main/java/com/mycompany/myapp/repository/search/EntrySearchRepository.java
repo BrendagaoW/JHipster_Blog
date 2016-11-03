a3028b8c2de3e4bdb0670b73c7e330b1e0623907
Brena <brendagao.lf@gmail.com>
2016-11-02 20:37:34 -0700

    Add Blog, Entry and Tag entities

---
+++ src/main/java/com/mycompany/myapp/repository/search/EntrySearchRepository.java
@@ -0,0 +1,10 @@
+package com.mycompany.myapp.repository.search;
+
+import com.mycompany.myapp.domain.Entry;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data ElasticSearch repository for the Entry entity.
+ */
+public interface EntrySearchRepository extends ElasticsearchRepository<Entry, Long> {
+}
